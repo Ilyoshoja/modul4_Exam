2e0a9b8da1a8f641f252ea885420e768580b5605
Ilyoshoja <imamashukurov@gmail.com>
2024-02-21 15:31:00 +0500

    2 son yig'indisini boshqa filga yozish

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        InputStream in = null;
+        FileWriter fileWrite = new FileWriter("sum.txt");
+        in = new FileInputStream("sonlar.txt");
+        Scanner scanner = new Scanner(in);
+        while (scanner.hasNext()) {
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            int sum = a + b;
+            System.out.println(a);
+            System.out.println(b);
+            System.out.println(a + b);
+            fileWrite.write("" + sum);
+
+        }
+        fileWrite.close();
+
+
+    }
+}
